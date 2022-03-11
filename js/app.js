ff89e17d1dd70e23782dd6d14ccffe8562194888
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2022-03-11 11:56:58 -0400

    feat: Add get random animals

---
+++ js/app.js
@@ -0,0 +1,47 @@
+let cat = document.querySelector('.cat');
+let fox = document.querySelector('.fox');
+let dog = document.querySelector('.dog');
+
+const catBtn = document.querySelector('.get-cat');
+const foxBtn = document.querySelector('.get-fox');
+const dogBtn = document.querySelector('.get-dog');
+
+catBtn.addEventListener('click', getRandomCat);
+foxBtn.addEventListener('click', getRandomFox);
+dogBtn.addEventListener('click', getRandomDog);
+
+function getRandomCat() {
+  fetch('https://aws.random.cat/meow')
+    .then((response) => response.json())
+    .then(({file}) => {
+      cat.innerHTML = `<img class="item" src="${file}" alt="" />`;
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    });
+}
+
+function getRandomFox() {
+  fetch('https://randomfox.ca/floof/')
+    .then(response => response.json())
+    .then(({image}) => {
+      fox.innerHTML = `<img class="item" src="${image}" alt="" />`
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    });
+}
+
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
+    .then(response => response.json())
+    .then(({url}) => {
+      dog.innerHTML = `<img class="item" src="${url}" alt="" />`
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    });
+}
